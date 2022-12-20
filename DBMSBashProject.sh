656f936f80b9c3cfb48c1d29bf9d4785d1449032
AhmedSamyyx <ahmed.samy.salahh@gmail.com>
2022-12-21 00:37:43 +0200

    Adding Update table, Select by Column name

--- DBMSBashProject.sh
+++ DBMSBashProject.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 clear
 
@@ -322,9 +322,7 @@ fi
         echo -e "*----------------------------------------------*\n"
          SelectMenu
     }
-    function SelectByColName {
-        SelectMenu
-    }
+
     function SelectByCondition {
         echo "Enter the table name :"
 	    read TableName
@@ -372,10 +370,95 @@ fi
         SelectMenu
     }
 
-# function UpdateTable {
 
-# }
+function SelectByColName {
+	echo "Enter Table Name :"
+	read tableName
+	echo "Enter Column Name :"
+	read colName
+	########################## 
+if [[ $colName = "" ]]
+then
+echo "You Can't Enter Empty Space!"
+else
+field=$(
+    awk '
+    BEGIN{FS="|"}
+        {
+            if(NR==1)
+                {
+                    for(i=1;i<=NF;i++)
+                    {
+                        if("'$colName'"==$i) print i
+                    }
+                }
+        }' ./$tableName/$tableName)
+awk 'BEGIN{FS=":"; ORS = " \n "}{print  $'$colName'}' ./$tableName/$tableName | tail -n +2
 
+
+
+
+
+
+
+	#awk 'BEGIN{FS=":"; ORS = " \n "}{print  $'$colNum'}' ./$tableName/$tableName
+
+
+
+
+	# awk 'BEGIN{FS=":"}{print $'$colname'}' ./$tableName/$tableName
+
+	#sep="|"
+	#	 awk 'BEGIN{FS=":"}{ print $'$colName'}' ./$tableName/$tableName
+	#awk 'BEGIN{FS="|"}{ if (NR==sep) print "Column"}' ./$tableName/$tableName
+
+
+	#sed -n '|d'
+fi	
+        SelectMenu
+    }
+
+
+function UpdateTable {
+	
+	echo  "Enter Table Name: "
+  read tableName
+  echo  "Enter Column name: "
+  read field
+  checkfid=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field'") print i}}}' ./$tableName/$tableName)
+  if [[ $checkfid == "" ]]
+  then
+    echo "Not Found"
+    TableMenu
+  else
+    echo "Enter The Condition Value: "
+    read value
+    result=$(awk 'BEGIN{FS=":"}{if ($'$checkfid'=="'$value'") print $'$checkfid'}' ./$tableName/$tableName 2>>./.error.log)
+    if [[ $result == "" ]]
+    then
+      echo "Value Not Found"
+      TableMenu
+    else
+      echo  "Enter Column name to set: "
+      read setField
+      setFid=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$setField'") print i}}}' ./$tableName/$tableName)
+      if [[ $setFid == "" ]]
+      then
+        echo "Not Found"
+        TableMenu
+      else
+        echo -e "Enter new Value to set: \c"
+        read newValue
+        NR=$(awk 'BEGIN{FS=":"}{if ($'$checkfid' == "'$value'") print NR}' ./$tableName/$tableName 2>>./.error.log)
+        oldValue=$(awk 'BEGIN{FS=":"}{if(NR=='$NR'){for(i=1;i<=NF;i++){if(i=='$setFid') print $i}}}' ./$tableName/$tableName 2>>./.error.log)
+        echo $oldValue
+        sed -i ''$NR's/'$oldValue'/'$newValue'/g' ./$tableName/$tableName 2>>./.error.log
+        echo "Row Updated Successfully"
+        TableMenu
+      fi
+    fi
+  fi
+}
 
 MainMenu
 
